027461429f4866ded5d2c8ed011f2b539532bb9f
lariddsouza <larissaddsouza@gmail.com>
2023-03-15 20:32:53 -0300

    codigo var e conversões

---
+++ conversoes.js
@@ -0,0 +1,18 @@
+// tipo de dado
+// como trabalhar isso com os booleanos
+
+// conversão implícia
+const numero = 5;
+const numeroString = Number("5");
+
+// comparação de tipos de variaveis
+console.log(numero === numeroString);
+// comparação de "conteúdo" de dentro das variáveis, que seria a comparação implícita
+console.log(numero == numeroString);
+// concatenação
+console.log(numero + numeroString);
+
+// conversão explícita
+// de string para número - Number()
+// de número para string - String()
+console.log(numero + numeroString);
