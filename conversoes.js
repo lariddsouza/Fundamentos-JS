63fc5ee3c948fe8d276b3538983bc99c45616701
lariddsouza <larissaddsouza@gmail.com>
2023-03-20 20:20:14 -0300

    outros exemplos

--- conversoes.js
+++ conversoes.js
@@ -16,3 +16,19 @@ console.log(numero + numeroString);
 // de string para número - Number()
 // de número para string - String()
 console.log(numero + numeroString);
+
+// == comparação implicita
+const numero1 = 5;
+const texto1 = "5";
+
+console.log(numero1 == texto1);
+
+// === comparação explicita;
+console.log(numero1 === texto1);
+
+//typeof
+console.log(typeof numero1);
+console.log(typeof texto1);
+
+// == compara só o valor
+// === compara o valor e o tipo de dado
---
+++ operador-ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCLiente = 19;
+
+// if (idadeCLiente >= idadeMinima) {
+//     console.log("cerveja")
+// }
+// else{
+//     console.log("suco")
+// }
+         //conndição                  //true        //false
+console.log(idadeCLiente >= idadeMinima ? "cerveja" : "suco"); 
